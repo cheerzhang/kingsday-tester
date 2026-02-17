0b9101197ba45a862b5db5c49f33c6c6613da85d
cheerzhang <zhanglenus@gmail.com>
2026-02-17 09:27:54 +0100

    delete file

--- jsapp/engine/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  gameLogic: require('./gameLogic'),
-  coreLogic: require('./coreLogic'),
-  victoryChecks: require('./victoryChecks'),
-};
--- pyapp/main.py
+++
@@ -1,5 +0,0 @@
-from ui.app import MainApp
-
-if __name__ == "__main__":
-    app = MainApp()
-    app.mainloop()
